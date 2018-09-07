1687bf3240f303d4a78656d1169f769d7dcaa51b
Jingsi <lookisliu@gmail.com>
2018-09-07 19:45:24 +0800

    move dates to utils

--- src/main/java/com/silveryark/authentication/AuthenticationApplication.java
+++ src/main/java/com/silveryark/authentication/AuthenticationApplication.java
@@ -3,6 +3,7 @@ package com.silveryark.authentication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//启动类，annotation的配置文件都放这里
 @SpringBootApplication(scanBasePackages = "com.silveryark")
 public class AuthenticationApplication {
 
--- src/main/java/com/silveryark/authentication/SecurityConfig.java
+++ src/main/java/com/silveryark/authentication/SecurityConfig.java
@@ -1,26 +1,28 @@
 package com.silveryark.authentication;
 
-import com.silveryark.authentication.util.Dates;
 import com.silveryark.rpc.RPCHttpHeaders;
 import com.silveryark.rpc.RPCResponse;
 import com.silveryark.rpc.authentication.AuthorizeRequest;
-import com.silveryark.rpc.authorize.AuthorizeResponse;
+import com.silveryark.rpc.authentication.AuthorizeResponse;
 import com.silveryark.security.JwtSecurityService;
-import org.apache.commons.lang3.time.DateUtils;
+import com.silveryark.utils.Dates;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.codec.CodecConfigurer;
 import org.springframework.http.codec.HttpMessageWriter;
-import org.springframework.http.codec.ServerCodecConfigurer;
 import org.springframework.security.authentication.UserDetailsRepositoryReactiveAuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
 import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.WebFilterExchange;
 import org.springframework.security.web.server.authentication.AuthenticationWebFilter;
 import org.springframework.security.web.server.authentication.ServerAuthenticationFailureHandler;
 import org.springframework.security.web.server.authentication.ServerAuthenticationSuccessHandler;
@@ -33,11 +35,16 @@ import org.springframework.web.reactive.result.view.ViewResolver;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 
 @EnableWebFluxSecurity
 public class SecurityConfig {
+
+    private static final int EXPIRES_DAYS = 30;
 
     @Bean
     public SecurityWebFilterChain securitygWebFilterChain(ServerHttpSecurity http,
@@ -61,14 +68,15 @@ public class SecurityConfig {
     @Bean
     public AuthenticationWebFilter authenticationWebFilter(
             UserDetailsRepositoryReactiveAuthenticationManager reactiveAuthenticationManager,
-            ServerCodecConfigurer configurer,
+            CodecConfigurer configurer,
             ServerAuthenticationSuccessHandler successHandler,
             ServerAuthenticationFailureHandler failureHandler) {
         AuthenticationWebFilter authenticationWebFilter = new AuthenticationWebFilter(reactiveAuthenticationManager);
         authenticationWebFilter.setAuthenticationConverter((ServerWebExchange exchange) -> {
             //这里使用 ServerRequest，就可以直接使用BodyExtractor来处理信息，否则还要根据http header来判断数据格式，
             ServerRequest serverRequest = ServerRequest.create(exchange, configurer.getReaders());
-            Mono<AuthorizeRequest> authorizeRequestMono = serverRequest.body(BodyExtractors.toMono(AuthorizeRequest.class));
+            Mono<AuthorizeRequest> authorizeRequestMono =
+                    serverRequest.body(BodyExtractors.toMono(AuthorizeRequest.class));
             return authorizeRequestMono.map((AuthorizeRequest request) -> {
                 //直接转成AuthenticationToken
                 AuthorizeRequest.Credential payload = request.getPayload();
@@ -85,9 +93,10 @@ public class SecurityConfig {
         return authenticationWebFilter;
     }
 
+    //失败的时候返回相应异常
     @Bean
-    public ServerAuthenticationFailureHandler failureHandler(ServerCodecConfigurer configurer) {
-        return ((webFilterExchange, exception) -> {
+    public ServerAuthenticationFailureHandler failureHandler(CodecConfigurer configurer) {
+        return ((WebFilterExchange webFilterExchange, AuthenticationException exception) -> {
             ServerWebExchange exchange = webFilterExchange.getExchange();
             //取出来requestId，为了构建response
             String requestId = exchange.getRequest().getHeaders().get(RPCHttpHeaders.REQUEST_ID).get(0);
@@ -95,6 +104,7 @@ public class SecurityConfig {
                     .ok()
                     .body(BodyInserters.fromObject(new AuthorizeResponse(requestId, RPCResponse.STATUS.OK,
                             exception)))
+                    //通过 configurer获取writer，来"智能"地完成消息转换
                     .flatMap((ServerResponse response) -> response.writeTo(exchange, new ServerResponse.Context() {
                         @Override
                         public List<HttpMessageWriter<?>> messageWriters() {
@@ -103,7 +113,7 @@ public class SecurityConfig {
 
                         @Override
                         public List<ViewResolver> viewResolvers() {
-                            return null;
+                            return Collections.emptyList();
                         }
                     }));
         });
@@ -112,12 +122,15 @@ public class SecurityConfig {
     //把成功认证的信息写到输出里（返回 jwtToken）
     @Bean
     public ServerAuthenticationSuccessHandler successHandler(JwtSecurityService jwtSecurityService, Dates dates,
-                                                             ServerCodecConfigurer configurer) {
-        Date now = dates.now();
-        Date expiredAt = DateUtils.addDays(now, 30);
-        return (webFilterExchange, authentication) -> {
+                                                             CodecConfigurer configurer) {
+        LocalDate now = dates.now().toLocalDate();
+        //默认过期时间30天
+        LocalDate expiredAt = now.plusDays(EXPIRES_DAYS);
+        return (WebFilterExchange webFilterExchange, Authentication authentication) -> {
             UsernamePasswordAuthenticationToken token = (UsernamePasswordAuthenticationToken) authentication;
-            String jwtToken = jwtSecurityService.encode(token.getName(), token.getAuthorities(), now, expiredAt);
+            String jwtToken = jwtSecurityService.encode(token.getName(), token.getAuthorities(),
+                    Date.from(now.atStartOfDay().atZone(ZoneId.systemDefault()).toInstant()),
+                    Date.from(expiredAt.atStartOfDay().atZone(ZoneId.systemDefault()).toInstant()));
             ServerWebExchange exchange = webFilterExchange.getExchange();
             //取出来requestId，为了构建response
             String requestId = exchange.getRequest().getHeaders().get(RPCHttpHeaders.REQUEST_ID).get(0);
@@ -133,7 +146,7 @@ public class SecurityConfig {
 
                         @Override
                         public List<ViewResolver> viewResolvers() {
-                            return null;
+                            return Collections.emptyList();
                         }
                     }));
         };
@@ -141,7 +154,8 @@ public class SecurityConfig {
 
     //用户数据
     @Bean
-    public UserDetailsRepositoryReactiveAuthenticationManager authenticationManager(ReactiveUserDetailsService userDetailsService) {
+    public UserDetailsRepositoryReactiveAuthenticationManager authenticationManager(
+            ReactiveUserDetailsService userDetailsService) {
         return new UserDetailsRepositoryReactiveAuthenticationManager(userDetailsService);
     }
 
@@ -153,6 +167,7 @@ public class SecurityConfig {
                 .password("password")
                 .roles("USER")
                 .build();
+        //只有admin role 的话是不可以访问 user数据的，如果需要user的role，需要显式地加到roles里
         UserDetails admin = User.withDefaultPasswordEncoder()
                 .username("admin")
                 .password("password")
